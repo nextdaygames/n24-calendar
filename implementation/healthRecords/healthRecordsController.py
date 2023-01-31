9c6c49e3b61cf58451b5cc1540cf8b4ecf5d9bfd
Oliver Barnum <oliverbarnum32@gmail.com>
2023-01-31 10:31:20 -0800

    Remove double cooors

--- implementation/healthRecords/healthRecordsController.py
+++ implementation/healthRecords/healthRecordsController.py
@@ -1,17 +1,14 @@
 from flask import Blueprint, request
-from flask_cors import cross_origin
 
 healthRecordsController = Blueprint('healthRecordsController',__name__)
 
 from implementation.healthRecords import healthRecordsDynamoAccess
 
 @healthRecordsController.route('/health-records', methods = ['GET'])
-@cross_origin
 def getHealthRecords():
     return { "healthRecords": healthRecordsDynamoAccess.getHealthRecords()}, 200
 
 @healthRecordsController.route('/health-records', methods = ['POST'])
-@cross_origin
 def createHealthRecord():
     body = request.get_json()
 
